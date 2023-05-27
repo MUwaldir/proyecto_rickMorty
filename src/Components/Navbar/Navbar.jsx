3ab5c74391539681d9e79f822fef0ec2db3945e9
Waldir merjildo <muwaldir@gmail.com>
2023-05-27 11:16:50 -0500

    avance con menu hamburguesa

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Onsearch from '../Onsearch/Onsearch';
 import { NavLink } from 'react-router-dom';
 import "./Navbar.css";
 function Navbar(props) {
     const isActive = true;
-    
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
     
     return ( 
    
      <div className="NavContainer">
-     <div className="btnContainer">
+        <div className="nav-container">
+
+        {/* <div className='nav-logo'>Rick</div> */}
+        <button className="hamburger" onClick={toggleMenu}>
+        <div className={`line ${isOpen ? 'line-open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'line-open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'line-open' : ''}`}></div>
+      </button>
+     <div className={`nav-menu ${isOpen ? 'nav-menu-open' : ''}`}>
      <NavLink  to={'/favorites'} className={isActive ? 'nav-link' : ''}  ><button className="nav_btn" >Favorites</button></NavLink>
      <NavLink  to={'/home'} className={isActive ? 'nav-link' : ''}  ><button className="nav_btn" >Home</button></NavLink>
      <NavLink  to={'/About'} className={isActive ? 'nav-link' : ''}  ><button className="nav_btn" >About</button></NavLink>
      </div>
+        </div>
+
      <div className="searchContainer">
      <Onsearch onSearch={props.idRick} />
 
